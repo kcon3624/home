e1d53f4a7ec257e0f85313d0007f359fa16a92eb
SEOK KIM <116332236+kcon3624@users.noreply.github.com>
2022-11-02 11:15:48 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    renderCurrentTime();
+    renderQuote();
+});
+
+//현재 시간
+function renderCurrentTime() {
+    let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let datetime = data['datetime'].substr(11,5);
+            $('#time').text(datetime);
+        });
+}
+
+//명언
+function renderQuote() {
+    let url = `https://api.quotable.io/random`;
+    fetch(url) 
+        .then(res => res.json()).then((data) => {
+            let content = `" ${data['content']} "`;
+            let author = `- ${data['author']} -`;
+            $('#content').text(content);
+            $('#author').text(author);
+        });
+}
